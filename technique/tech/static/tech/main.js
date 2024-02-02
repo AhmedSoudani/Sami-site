c894e540d68c3692817980d882e1cc0755bfea07
SPARATA <ahmedsoudani09@gmail.com>
2024-02-02 20:20:16 +0100

    the swap function works fine for now :)

--- technique/tech/static/tech/main.js
+++ technique/tech/static/tech/main.js
@@ -1,38 +1,58 @@
 document.addEventListener("DOMContentLoaded", function () {
+  select1 = document.getElementById("from");
+  select2 = document.getElementById("to");
+
   document.getElementById("swap").onclick = function (event) {
-    let indexto = from;
-    let indexfrom = to;
-    select1.selectedIndex = indexfrom;
-    select2.selectedIndex = indexto;
-  };
+    if (document.getElementById("from").selectedIndex != 0 &&
+     document.getElementById("to").selectedIndex != 0 && 
+     document.getElementById("from").selectedIndex != document.getElementById("to").selectedIndex
+     ){
+      swapOptions();
+    }
+    else {
+      const element = document.getElementById("converted");
+      element.style.display = "block";
+      element.style.animationPlayState = "running";
+      element.innerHTML =
+    "Pick Two bases to swap!";
+    }
+  }
+
   document.getElementById("form1").onsubmit = function (event) {
     event.preventDefault();
-    var select1 = document.getElementById("from");
-    var select2 = document.getElementById("to");
-    var from = document.getElementById("from").selectedIndex;
-    var to = document.getElementById("to").selectedIndex;
-    var options = document.getElementById("from").options;
-    var number = document.getElementById("number").value;
-
+    select1 = document.getElementById("from");
+    select2 = document.getElementById("to");
+    from = document.getElementById("from").selectedIndex;
+    to = document.getElementById("to").selectedIndex;
+    options = document.getElementById("from").options;
+    
+    number = document.getElementById("number").value;
     if (options[to].text == "Decimal (Base 10)") {
+
       convertBase(number, "10", options[from].value);
 
       const element = document.getElementById("converted");
       element.style.display = "block";
       element.style.animationPlayState = "running";
+
     } else if (options[to].text == "Binary (Base 2)") {
+
       convertBase(number, "2", options[from].value);
 
       const element = document.getElementById("converted");
       element.style.display = "block";
       element.style.animationPlayState = "running";
+
     } else if (options[to].text == "Octal (Base 8)") {
+
       convertBase(number, "8", options[from].value);
 
       const element = document.getElementById("converted");
       element.style.display = "block";
       element.style.animationPlayState = "running";
+
     } else if (options[to].text == "Hexadecimal (Base 16)") {
+
       convertBase(number, "16", options[from].value);
 
       const element = document.getElementById("converted");
@@ -40,10 +60,23 @@ document.addEventListener("DOMContentLoaded", function () {
       element.style.animationPlayState = "running";
     }
   };
+
+  
 });
+
+function swapOptions() {
+  let indexFrom = select1.selectedIndex;
+  let indexTo = select2.selectedIndex;
+
+  // Swap the selected indices
+  select1.selectedIndex = indexTo;
+  select2.selectedIndex = indexFrom;
+}
+
 
 function convertBase(nb, b2, b1) {
   // Convert the input number to decimal
+
   let decimalNumber = parseInt(nb, b1);
 
   // Convert the decimal number to the desired base
