f7f25b626152909471df3892d88de0fe56cc37d4
SPARATA <ahmedsoudani09@gmail.com>
2024-02-02 17:05:31 +0100

    still fixing the swap function!

--- technique/tech/static/tech/main.js
+++ technique/tech/static/tech/main.js
@@ -1,10 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("swap").onclick = function (event) {
+    let indexto = from;
+    let indexfrom = to;
+    select1.selectedIndex = indexfrom;
+    select2.selectedIndex = indexto;
+  };
   document.getElementById("form1").onsubmit = function (event) {
     event.preventDefault();
-    let from = document.getElementById("from").selectedIndex;
-    let to = document.getElementById("to").selectedIndex;
-    let options = document.getElementById("from").options;
-    let number = document.getElementById("number").value;
+    var select1 = document.getElementById("from");
+    var select2 = document.getElementById("to");
+    var from = document.getElementById("from").selectedIndex;
+    var to = document.getElementById("to").selectedIndex;
+    var options = document.getElementById("from").options;
+    var number = document.getElementById("number").value;
 
     if (options[to].text == "Decimal (Base 10)") {
       convertBase(number, "10", options[from].value);
@@ -36,16 +44,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function convertBase(nb, b2, b1) {
   // Convert the input number to decimal
-  console.log("function entred", b2, b1);
   let decimalNumber = parseInt(nb, b1);
-  console.log(decimalNumber);
 
   // Convert the decimal number to the desired base
   let result = decimalNumber.toString(b2);
-  console.log(result);
+
+  if (result == NaN) {
+    document.getElementById("converted").innerHTML = "Invalid input!";
+  }
 
   document.getElementById("converted").innerHTML =
-    "The number: " + result + " is in Binary.";
-
-  console.log("went to converted");
+    "The number: " + result + " is in Base (" + options[to].value + ")";
 }
